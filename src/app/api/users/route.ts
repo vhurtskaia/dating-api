2dc8cfcd465e5c72da0ded627402e9ad2684a71c
vazha <gudenaffalkon@gmail.com>
2025-03-19 21:14:22 +0100

    fix

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+interface IUser {
+    address: string;
+    age: number;
+    city: string;
+    count_photos: number;
+    count_videos: number;
+    gender: 'male' | 'female';
+    height: number;
+    id: number;
+    is_online: boolean;
+    name: string;
+    phone: string;
+    slug: string;
+    telegram: string | null;
+    weight: number;
+    whatsapp: string;
+    location?: string;
+    hobbies?: string[];
+}
+
+const parseJSONL = (filePath: string): IUser[] => {
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileData.trim().split("\n");
+    return lines.map(line => JSON.parse(line));
+};
+
+const filePath = path.join(process.cwd(), 'public', 'profiles.jsonl');
+const users: IUser[] = parseJSONL(filePath);
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const gender = searchParams.get('gender');
+    const location = searchParams.get('location');
+    const hobby = searchParams.get('hobby');
+
+    const filteredUsers = users.filter((user) => {
+        return (
+            (!gender || user.gender === gender) &&
+            (!location || user.location?.toLowerCase().includes(location?.toLowerCase() || '')) &&
+            (!hobby || user.hobbies?.includes(hobby || ''))
+        );
+    });
+
+    return NextResponse.json(filteredUsers);
+}
