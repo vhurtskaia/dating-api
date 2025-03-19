71906bb03ce5e709ba95bfe6a0164252ae6cbcaa
vazha <gudenaffalkon@gmail.com>
2025-03-19 21:21:30 +0100

    fix

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+
+  console.log()
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+      </main>
+    </div>
+  );
+}
